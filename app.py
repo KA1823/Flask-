d0070eb8fc2caa619b3236e1b8ba4a5f99cb3a58
Kamran Anwar <kamran.anwat091@gmail.com>
2022-11-27 23:48:58 +0500

    Commented

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask
-# request
-app = Flask(__name__)
+# from flask import Flask
+# # request
+# app = Flask(__name__)
 
 
 
-@app.route("/")
-def hello():
-   # print (request.headers)
-   return "Welcome Flask APP"
+# @app.route("/")
+# def hello():
+#    # print (request.headers)
+#    return "Welcome Flask APP"
 
 
 
-# if __name__ == "__main__":
-#    app.run()
+# # if __name__ == "__main__":
+# #    app.run()
 

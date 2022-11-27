0dd3c1d5bfe5f73995c0c048616f01155e22aa5e
Kamran Anwar <kamran.anwat091@gmail.com>
2022-11-28 01:03:23 +0500

    commited

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
-from flask import Flask, request
+from flask import Flask
+# request
 app = Flask(__name__)
 
 
 
 @app.route("/")
 def hello():
-   print (request.headers)
+   # print (request.headers)
    return "Welcome Flask APP"
 
 
 
-if __name__ == "__main__":
-   app.run()
-=======
-# from flask import Flask
-# # request
-# app = Flask(__name__)
-
-
-
-# @app.route("/")
-# def hello():
-#    # print (request.headers)
-#    return "Welcome Flask APP"
-
-
-
-# # if __name__ == "__main__":
-# #    app.run()
->>>>>>> d0070eb8fc2caa619b3236e1b8ba4a5f99cb3a58
+# if __name__ == "__main__":
+#    app.run()
 
